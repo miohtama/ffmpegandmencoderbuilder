86074fe34d9f6c75d620f85f7daf6cda6df31c8a
Mikko Ohtamaa <moo@mail.twinapex.fi>
2010-01-03 03:17:27 +0200

    Fixed GPAC building on Linux

--- fixes.py
+++ fixes.py
@@ -7,15 +7,19 @@ import os
 import sys
 
 
-def fix_gpac(options, buildout):
+def fix_gpac_configure(options, buildout):
 	""" Fix gpac building """
 	os.system("chmod u+x configure")
 	if sys.platform == "darwin": # OSX specific fix
 		print "Patching gpac for OSX"
 		os.system("find . -name os_net.c | xargs perl -pi -e 's/u_long/unsigned long/g'")
-	elif sys.platform == "linux":
+	print "Done"
+
+def fix_gpac_make(options, buildout):
+	""" Fix ugly Mozilla dependency which cannot be disabled via configure """
+	if sys.platform == "linux2":
 		print "Patching gpac for Linux"
-		os.system("find . -name applications/Makefile | xargs perl -pi -e 's/INSTDIRS+=osmozilla/#INSTDIRS+=osmozilla/g'")
-		os.system("find . -name applications/Makefile | xargs perl -pi -e 's/APPDIRS+=osmozilla/#APPDIRS+=osmozilla/g'")
+		os.system("find . -print0 | grep -FzZ applications/Makefile | xargs perl -pi -e 's/INSTDIRS\\+\\=osmozilla/#INSTDIRS+=osmozilla/g'")
+		os.system("find . -print0 | grep -FzZ applications/Makefile | xargs perl -pi -e 's/APPDIRS\\+\\=osmozilla/#APPDIRS+=osmozilla/g'")
 			
 	print "Done"
